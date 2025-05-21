c894f1fa397e3899ba7f2ca51cd7422a6f207794
unmatched78 <marvinavi24@gmail.com>
2025-05-21 23:12:42 +0200

    again setatic in settings

--- note/settings.py
+++ note/settings.py
@@ -163,13 +163,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
-#for collectstatic whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# Media files (uploaded images, videos)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
+# #for collectstatic whitenoise
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # Media files (uploaded images, videos)
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
