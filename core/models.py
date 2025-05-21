0b175bb5c5751c3f101f2b2bd61d8bca48b499f5
unmatched78 <marvinavi24@gmail.com>
2025-05-21 23:02:41 +0200

    dded clodunary to model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 class Timer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
