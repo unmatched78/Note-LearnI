56f519759bd8d44e02f104d95fc6bc7b7d1f5191
unmatched78 <marvinavi24@gmail.com>
2025-05-25 12:08:43 +0200

    clean

--- note/urls.py
+++ note/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf import settings
-#from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+#from django.conf import settings
+#from django.conf.urls.static import static
 # serve static files and media
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
