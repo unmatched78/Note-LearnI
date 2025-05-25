4867bb588402a6e31b90c9d3dbabc9895344621e
unmatched78 <marvinavi24@gmail.com>
2025-05-25 12:07:22 +0200

    clean

--- note/settings.py
+++ note/settings.py
@@ -9,7 +9,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Your Gmail address  
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')   
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
