d2343a4691ae4dd97f980c1ad1019a358f3fbf11
unmatched78 <marvinavi24@gmail.com>
2025-05-25 12:07:05 +0200

    clean

--- note/settings.py
+++ note/settings.py
@@ -10,7 +10,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Your Gmail address  
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use an App Password for increased security  
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Quick-start development settings - unsuitable for production
