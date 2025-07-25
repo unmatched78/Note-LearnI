4fc931c056fe9108c722a8686c538fb0cf862cd9
unmatched78 <marvinavi24@gmail.com>
2025-07-26 01:16:16 +0200

    from shortuuidfield import ShortUUIDField

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 import json
 from django.utils import timezone
-import shortuuid
+from shortuuidfield import ShortUUIDField
 
 def validate_json(value):
     """
