a6b5fde2ff764e2ba5c880dc549b6b0738593fd9
unmatched78 <marvinavi24@gmail.com>
2025-07-26 18:46:59 +0200

    import { ClerkProvider } from '@clerk/clerk-react'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from './context/AuthContext';
-
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -13,11 +13,13 @@ if (!PUBLISHABLE_KEY) {
 }
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
   <AuthProvider>
   <StrictMode>
     <App />
   </StrictMode>
   </AuthProvider>
+  </ClerkProvider>
   </ThemeProvider>
   ,
 )
