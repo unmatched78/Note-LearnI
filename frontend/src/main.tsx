5339c6fccf554d69a91c63da3e51fe7fa7721668
unmatched78 <marvinavi24@gmail.com>
2025-07-26 18:45:32 +0200

    @@update: the start of clerk integrations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from './context/AuthContext';
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
   <AuthProvider>
