393b22228f944c2f5f8582e4d7a028a3bd2280fe
unmatched78 <marvinavi24@gmail.com>
2025-07-26 15:15:11 +0200

    fix:bacl to old way fetching profile via users/me

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,6 @@ from .views import (
     QuizViewSet,
     QuizAttemptViewSet,
     UserViewSet,
-    MyTokenRefreshView,
 )
 
 router = DefaultRouter()
@@ -23,8 +22,7 @@ urlpatterns = [
     # 1) Public /auth endpoints FIRST
     path('api/auth/register/',       RegisterView.as_view(),       name='auth_register'),
     path('api/auth/token/login/',    MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    #path('api/token/refresh/',  TokenRefreshView.as_view(),      name='token_refresh'),
-    path("api/auth/refresh/", MyTokenRefreshView.as_view(),     name="token_refresh"),
+    path('api/token/refresh/',  TokenRefreshView.as_view(),      name='token_refresh'),
 
     # 2) Then everything else under /api/
     path('api/', include(router.urls)),
